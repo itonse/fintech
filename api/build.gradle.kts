bdae95cac217773c56bd0c8c7b34470546844f94
itonse <govl3dnjs@gmail.com>
2023-05-15 21:55:39 +0900

    Log Aspect 구현

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {}
 version = "0.0.1"
 
 dependencies {
+    // spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
@@ -11,7 +12,14 @@ dependencies {
 
     implementation(project(":domain"))   // domain 모듈 사용
 
+    // test 관련
     testImplementation("io.mockk:mockk:1.12.0")    // 코틀린 테스트 프레임워크 (단위테스트)
     runtimeOnly("com.h2database:h2")   // 단위 테스트에서 사용할 DB (로컬 테스트가 돌아가도 실제 DB 에는 영향X)
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")  // mapper 관련
+
+    // AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    // Logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")  // 코틀린 로깅 사용
 }
---
+++ api/src/main/kotlin/com/example/api/aop/LogAspect.kt
@@ -0,0 +1,34 @@
+package com.example.api.aop
+
+import mu.KotlinLogging
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.stereotype.Component
+import org.springframework.util.StopWatch
+
+@Aspect
+@Component
+class LogAspect {
+    val logger = KotlinLogging.logger { }
+
+    // api 모듈인지를 확인하는 Join points 를 가져오는 Pointcut
+    @Pointcut("within(com.example.api..*)")  // api 패키지 내의 메소드들이 실행될 때 모두 로그를 찍음
+    private fun isApi() {}
+
+    @Around("isApi()")
+    fun loggingAspect(joinPoint: ProceedingJoinPoint): Any {   // join Point 를 가져와서 그 다음 join Point 로 넘김
+        val stopwatch = StopWatch()
+        stopwatch.start()   // 스탑워치 시작
+
+        val result = joinPoint.proceed()   // joinPoint 를 실행해서 응답값을 받음
+
+        stopwatch.stop()   // 실행이 완료되면 스탑워치 종료
+
+        // 로그 찍기: jointPoint 의 이름, jointPoint 에 어떤 args 가 들어왔는지, 해당 joinPoint 를 실행할 때 걸린 시간
+        logger.info { "${joinPoint.signature.name} ${joinPoint.args[0]} ${stopwatch.lastTaskTimeMillis}" }
+
+        return result
+    }
+}
